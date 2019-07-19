20b11f01c6e560640eb9ebe2a634be290008cc78
Hoxtygen <hoxtygen@live.com>
2019-07-19 09:27:57 +0100

    Get a project

--- model/helpers/pojectModel.js
+++ model/helpers/pojectModel.js
@@ -6,7 +6,29 @@ function get() {
   return db('projects');
 }
 
+function getById(id) {
+  return db('projects').where({ id });
+}
+
+async function add(projectData) {
+  const id = await db('schemes')
+    .insert(projectData);
+  return getById(id);
+}
+
 
 module.exports = {
   get,
+  getById,
+  add,
 };
+
+
+/*
+
+
+function insert(project) {
+  return db('projects')
+    .insert(project)
+    .then(([id]) => this.get(id));
+} */
--- model/routes/projectRoutes.js
+++ model/routes/projectRoutes.js
@@ -5,12 +5,28 @@ const projects = require('../helpers/pojectModel.js');
 
 router.get('/', async (req, res) => {
   try {
-    const projectsData = await projects.get();
+    const [projectsData] = await projects.get();
     res.status(200).json(projectsData);
   } catch (error) {
     res.status(500).json({
       errorMessage: error,
     });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const [project] = await projects.getById(id);
+
+    if (project) {
+      res.json(project);
+    } else {
+      res.status(404).json({ message: 'Could not find project with given id.' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get projects' });
   }
 });
 
